356a722fb1416b372a3a6ff2fb23efb9a07f8384
Lennin Ibarra <ing.lenninibarra@gmail.com>
2025-05-12 15:28:46 -0500

    useEffect + useRef + props

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import { Counter } from './components/Counter';
 import { Usuario } from './components/Usuario';
 
+import { TimerPadre } from './components/TimerPadre';
+
 function App() {
   return (
     <div className="App">
       <h1>React con typescript</h1>
+      <h2>useState</h2>
       <hr />
       <Counter/>
       <Usuario/>
+
+      <h2 className='mt-5'>useEffect - useRef</h2>
+      <hr />
+      <TimerPadre/>
     </div>
   );
 }
